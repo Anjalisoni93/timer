ec8607c6c0d24aa2f2b971e732764f363553b5f6
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-17 12:04:15 -0700

    Added some comments for more understanding

--- timer1.js
+++ timer1.js
@@ -1,15 +1,20 @@
 const timer = function(time) {
+  // checking for edge cases negative number and not a number
   if (time < 0 || isNaN(time)) {
     return null;
   }
 
+  // the actual timer 
   setTimeout(() => {
     process.stdout.write('\x07');
   }, time * 1000);
 };
 
+// returns array containing arguments
 const argv = process.argv.slice(2);
 
+
+// iterates over the arguments 
 for (let i of argv) {
   timer(i);
 }
